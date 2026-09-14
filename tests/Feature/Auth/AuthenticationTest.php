884cd1a0fe0113b35829280ea26f9efcccabe38b
*DZCYJ9jMRm <haruki.shioya@asno-sys.co.jp>
2026-09-14 18:49:19 +0900

    テストパス修正

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -19,7 +19,7 @@ test('users can authenticate using the login screen', function () {
 
     $response
         ->assertSessionHasNoErrors()
-        ->assertRedirect(route('dashboard', absolute: false));
+        ->assertRedirect(route('dashboard-cafe', absolute: false));
 
     $this->assertAuthenticated();
 });
@@ -61,7 +61,7 @@ test('users can logout', function () {
 
     $response = $this->actingAs($user)->post(route('logout'));
 
-    $response->assertRedirect(route('home'));
+    $response->assertRedirect(config('fortify.home'));
 
     $this->assertGuest();
 });
